bc0f49d7cad178eacb601bc57e632553ffd2d7da
hybriddap <120157375+hybriddap@users.noreply.github.com>
2023-11-27 17:07:31 +1000

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,15 @@
+document.getElementById('Home').onclick = function(){
+    window.location.href="index.html";
+}
+
+document.getElementById('Projects').onclick = function(){
+    window.location.href="projects.html";
+}
+
+document.getElementById('Contact').onclick = function(){
+    window.location.href="contact.html";
+}
+
+document.getElementById("MITF").onclick=function(){
+    window.open("https://mrdap.itch.io/the-man-in-the-forest");
+}
