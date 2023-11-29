9f531fe2bc2562b93acc69a589171101d0ae201b
hybriddap <danielspresian@gmail.com>
2023-11-30 00:43:12 +1000

    Projects Added But Some Aren't Finished

---
+++ JavaScript/main.js
@@ -0,0 +1,23 @@
+document.getElementById('Home').onclick = function(){
+    window.location.href="index.html";
+}
+
+document.getElementById('Projects').onclick = function(){
+    window.location.href="projects.html";
+}
+
+document.getElementById('Contact').onclick = function(){
+    window.location.href="contact.html";
+}
+
+document.getElementById("MITF").onclick=function(){
+    window.open("https://mrdap.itch.io/the-man-in-the-forest");
+}
+
+document.getElementById("BlockC").onclick=function(){
+    window.open("https://drive.google.com/file/d/1Kk7T2hWqhxNPZ4I1rf-sU2hnhR9nyRMM/view?usp=sharing");
+}
+
+document.getElementById("MKR").onclick=function(){
+    window.open("https://drive.google.com/file/d/1JEqbD5T39uokifqp38eDJLxivu7T0oTJ/view?usp=sharing");
+}
